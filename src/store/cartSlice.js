4ed73437ab4a9a3f3345a8a38b3391e79331e397
muhammad faraz ali <muhammad.faraz@confiz.com>
2024-04-28 21:17:13 +0500

    Final Project using createAsyncThunk middleware

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -1,16 +1,28 @@
-const { createSlice } = require('@reduxjs/toolkit');
+const { createSlice } = require("@reduxjs/toolkit");
 
 const cartSlice = createSlice({
-    name: 'cart',
-    initialState: [],
-    reducers: {
-        add(state, action) {
-            state.push(action.payload);
-        },
-        remove(state, action) {
-            return state.filter((item) => item.id !== action.payload);
-        },
+  name: "cart",
+  initialState: [],
+  reducers: {
+    add(state, action) {
+      const productId = state.find(
+        (product) => product.id === action.payload.id
+      );
+      if (productId) {
+        state.find((product) => {
+          if (product.id === action.payload.id) {
+            return { ...product, count: product.count++ };
+          }
+          // return;
+        });
+      } else {
+        state.push({ ...action.payload, count: 1 });
+      }
     },
+    remove(state, action) {
+      return state.filter((item) => item.id !== action.payload);
+    },
+  },
 });
 
 export const { add, remove } = cartSlice.actions;
--- src/store/productSlice.js
+++ src/store/productSlice.js
@@ -1,61 +1,101 @@
-const { createSlice, createAsyncThunk } = require('@reduxjs/toolkit');
+const { createSlice, createAsyncThunk } = require("@reduxjs/toolkit");
 
 export const STATUSES = Object.freeze({
-    IDLE: 'idle',
-    ERROR: 'error',
-    LOADING: 'loading',
+  IDLE: "idle",
+  ERROR: "error",
+  LOADING: "loading",
 });
 
+export const fetchProducts = createAsyncThunk(
+  "product/fetchProducts", //Action type
+  async (args, { rejectWithValue }) => {
+    //Async func
+    console.log("index args", args);
+    try {
+      const res = await fetch("https://fakestoreapi.com/products");
+      const data = await res.json();
+      return data;
+    } catch (erro) {
+      return rejectWithValue("Error");
+    }
+  }
+);
+
 const productSlice = createSlice({
-    name: 'product',
-    initialState: {
-        data: [],
-        status: STATUSES.IDLE,
-    },
-    reducers: {
-        // setProducts(state, action) {
-        //     state.data = action.payload;
-        // },
-        // setStatus(state, action) {
-        //     state.status = action.payload;
-        // },
-    },
-    extraReducers: (builder) => {
-        builder
-            .addCase(fetchProducts.pending, (state, action) => {
-                state.status = STATUSES.LOADING;
-            })
-            .addCase(fetchProducts.fulfilled, (state, action) => {
-                state.data = action.payload;
-                state.status = STATUSES.IDLE;
-            })
-            .addCase(fetchProducts.rejected, (state, action) => {
-                state.status = STATUSES.ERROR;
-            });
-    },
+  name: "product",
+  initialState: {
+    data: [],
+    status: STATUSES.IDLE,
+  },
+  //   reducers: {
+  //     setProducts(state, action) {
+  //       console.log("index", action);
+  //       state.data = action.payload;
+  //     },
+  //     setStatus(state, action) {
+  //       state.status = action.payload;
+  //     },
+  //   },
+  extraReducers: (builder) => {
+    // Way-1 :Using builder API/tool
+    console.log("index builder", builder);
+    builder
+      .addCase(fetchProducts.pending, (state, action) => {
+        console.log("index-p", action);
+        console.log("index action", fetchProducts.pending);
+        state.status = STATUSES.LOADING;
+      })
+      .addCase(fetchProducts.fulfilled, (state, action) => {
+        state.data = action.payload;
+        state.status = STATUSES.IDLE;
+      })
+      .addCase(fetchProducts.rejected, (state, action) => {
+        state.status = STATUSES.ERROR;
+      });
+  },
+  //   extraReducers: {
+  //     //Way-2 (Define fetchProducts before creating a slice)
+  //     [fetchProducts.pending]: (state, action) => {
+  //       console.log("index action-p", action);
+  //       state.status = STATUSES.LOADING;
+  //     },
+  //     [fetchProducts.fulfilled]: (state, action) => {
+  //       console.log("index action-f", action);
+  //       state.data = action.payload;
+  //       state.status = STATUSES.IDLE;
+  //     },
+  //     [fetchProducts.rejected]: (state, action) => {
+  //       console.log("index action-r", action);
+  //       state.status = STATUSES.ERROR;
+  //     },
+  //   },
 });
 
 export const { setProducts, setStatus } = productSlice.actions;
 export default productSlice.reducer;
 
-// Thunks
-export const fetchProducts = createAsyncThunk('products/fetch', async () => {
-    const res = await fetch('https://fakestoreapi.com/products');
-    const data = await res.json();
-    return data;
-});
+//Thunk-By RTK
+// export const fetchProducts = createAsyncThunk(
+//   "product/fetchProducts",
+//   async () => {
+//     const res = await fetch("https://fakestoreapi.com/products");
+//     const data = await res.json();
+//     return data;
+//   }
+// );
 
+//Thunk by dev
 // export function fetchProducts() {
-//     return async function fetchProductThunk(dispatch, getState) {
-//         dispatch(setStatus(STATUSES.LOADING));
-//         try {
-//             const res = await fetch('https://fakestoreapi.com/products');
-//             const data = await res.json();
-//             dispatch(setProducts(data));
-//             dispatch(setStatus(STATUSES.IDLE));
-//         } catch (err) {
-//             console.log(err);
-//             dispatch(setStatus(STATUSES.ERROR));
-//         }
-//     };
+//   return async function fetchProductThunk(dispatch, getState) {
+//     dispatch(setStatus(STATUSES.LOADING));
+//     try {
+//       const res = await fetch("https://fakestoreapi.com/products");
+//       const data = await res.json();
+//       dispatch(setProducts(data));
+//       dispatch(setStatus(STATUSES.IDLE));
+//     } catch (err) {
+//       console.log(err);
+//       dispatch(setStatus(STATUSES.ERROR));
+//     }
+//   };
 // }
